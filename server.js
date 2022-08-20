0d6aafa5c263b43d630b3a5c780c79610a90f7d1
Chris Key <robertschris010@gmail.com>
2022-08-20 13:23:33 -0500

    updated connection

--- server.js
+++ server.js
@@ -19,13 +19,21 @@ app.use((req, res) => {
 
 
 //START SERVER
-db.connect(err => {
-    if (err) throw err;
-    console.log('Connected');
-    app.listen(PORT, () => {
-        console.log(`Server Running ${PORT}`);
-        promptUser();
-    });
+// db.connect(err => {
+//     if (err) throw err;
+//     console.log('Connected');
+//     app.listen(PORT, () => {
+//         console.log(`Server Running ${PORT}`);
+//         promptUser();
+//     });
+// });
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
 
