922321ff22a242fc059eb333823091d874e196cc
Chris Key <robertschris010@gmail.com>
2022-08-20 19:13:12 -0500

    added info to role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,14 @@ VALUES
 (2, 'Sales'),
 (3, 'Legal'),
 (4, 'Finance');
+
+INSERT INTO role ( id, title, salary, department_id)
+VALUES
+(1, 'Sales Lead', '140000', '2'),
+(2, 'Lead Engineer', '170000', '1'),
+(3, 'Accountant', '100000', '4'),
+(4, 'Software Engineer', '90000', '1'),
+(5, 'Lawyer', '200000', '3'),
+(6, 'Legal Team Lead', '210000', '3'),
+(7, 'Salesperson', '80000', '2'),
+(8, 'Account Manager', '120000', '4');
